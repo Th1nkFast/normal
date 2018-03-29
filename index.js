eaa581656b80e71efb196efbb4932f6fb0969a20
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2018-03-29 01:23:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,5 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port ' + config.port);
-});
+const port = process.env.PORT;
+app.listen(port);
